b0da4792e2a7c025a79fae8c634388aa940ed8ce
Muhammed Zameel C <muhammedzameelc1@gmail.com>
2025-07-04 10:55:26 +0530

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -79,14 +79,14 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# # ------------------ 📸 LOCAL BABY IMAGE ------------------ #
-# st.image("amish.jpg", caption="🎂 Little Birthday Star!", use_column_width=True)
+# # # ------------------ 📸 LOCAL BABY IMAGE ------------------ #
+# # st.image("amish.jpg", caption="🎂 Little Birthday Star!", use_column_width=True)
 
-# ------------------ 📦 GIFT BUTTON ------------------ #
-if st.button("🎁 Open Gift!"):
-    st.success("🎉 Surprise! Wishing you a life full of love and laughter, Amish! 💙")
-    st.balloons()
-    st.image("amish.jpg", caption="Here's a cute gift for you!", use_column_width=True)
+# # ------------------ 📦 GIFT BUTTON ------------------ #
+# if st.button("🎁 Open Gift!"):
+#     st.success("🎉 Surprise! Wishing you a life full of love and laughter, Amish! 💙")
+#     st.balloons()
+#     st.image("amish.jpg", caption="Here's a cute gift for you!", use_column_width=True)
 
 # ------------------ 🎈 FLOATING BALLOONS ANIMATION ------------------ #
 balloon_html = """
